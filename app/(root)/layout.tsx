125e5f3680387e07ed5f6ae5334a0e3a58c015af
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2023-08-22 18:47:51 +0530

    Starting layout and added assets

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,41 @@
+import { ClerkProvider } from "@clerk/nextjs/app-beta";
+import "../globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Header from "@/components/shared/Header";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Footer from "@/components/shared/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Threads Clone",
+  description: "A Next.js Threads Application",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Header />
+          <main className="flex">
+            <LeftSidebar />
+
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+
+            <RightSidebar />
+          </main>
+          <Footer />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
